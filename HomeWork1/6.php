37a36d86ca725060354b0ffae0b8573f0b96acfd
GuryanovaIrene <44869308+GuryanovaIrene@users.noreply.github.com>
2018-11-11 09:20:00 +0300

    Update 6.php

--- HomeWork1/6.php
+++ HomeWork1/6.php
@@ -2,7 +2,7 @@
 <table>
     <thead>
         <tr>
-            <th>&nbsp;</th>';
+            <th>&nbsp;</th>
             <?php
             for ($i = 1; $i <= 10; $i++) {
                 echo '<th>' . $i . '</th>';
