2c0ae992d7e2cf37e83a1c59a6972e6c1fe01a19
GuryanovaIrene <44869308+GuryanovaIrene@users.noreply.github.com>
2018-11-25 12:49:46 +0300

    Update 3.php

--- HomeWork1/3.php
+++ HomeWork1/3.php
@@ -5,7 +5,7 @@ if ($age >= 18 and $age <= 65) {
     echo "Вам еще работать и работать";
 } elseif ($age > 65) {
     echo "Вам пора на пенсию";
-} elseif ($age >= 1 and $age <= 17) {
+} elseif ($age >= 1 and $age < 18) {
     echo "Вам еще рано работать";
 } else {
     echo "Неизвестный возраст";
